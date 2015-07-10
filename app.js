f3502615164b1e583ef84c04f604700d08f19c52
brenten lovato <brentengust@gmail.com>
2015-07-10 11:55:14 -0700

    connected table to map

--- app.js
+++ app.js
@@ -11,41 +11,73 @@ DPWLayer.setGeoJSON(DPW);
 // On map move, determine if point lies within bounding box
 // run buffer on davy points and test which points lie within
 // add within dpw points to the html table
-map.on('move', function() {
-  var DavyInBounds = [],
-      DPWInBounds = [],
-      bounds = map.getBounds();
+var tableBodyDavy = document.getElementById('davy');
+var tableBodyDPW = document.getElementById('DPW');
 
-  // on each Davy layer calulate if DPW lies within buffer and append table head
+map.on('move', function() {
+  var bounds = map.getBounds();
+  //remove existing elements from table
+  while (tableBodyDavy.firstChild) {
+      tableBodyDavy.removeChild(tableBodyDavy.firstChild);
+  }
+  while (tableBodyDPW.firstChild) {
+    tableBodyDPW.removeChild(tableBodyDPW.firstChild);
+  }
+  // on each Davy layer calulate if DPW lies within bbox and append table
   DavyLayer.eachLayer(function(marker) {
     if (bounds.contains(marker.getLatLng())) {
-      DavyInBounds.push('<tr class="" id='+marker._leaflet_id+'><td>'+marker._leaflet_id+'</td><td>'+marker._leaflet_id+'</td><td>'+marker.feature.properties.name+'</td><td>Another Attribute</td></tr>');
-    }
-    $('#'+ marker._leaflet_id, 'tr').click(function(){
-      map.featureLayer.setFilter(function(f) {
-        return true;
-      });
-    });
-  });
-  // concatenate html and set it for Davy
-  DavyInBoundsConcat = "";
-  for(i=0; i < DavyInBounds.length; i++) {
-    DavyInBoundsConcat += DavyInBounds[i];
-  }
-  document.getElementById('davy').innerHTML = DavyInBoundsConcat;
+      var link = tableBodyDavy.appendChild(document.createElement('a'));
+      link.className = 'item';
+      link.href = '#';
+      link.innerHTML = '<tr class="" id='+marker._leaflet_id+'><td>'+marker._leaflet_id+'</td><td>'+marker.feature.properties.name+'</td><td>Davy</td></tr><br>';
+      link.onclick = function() {
+        if (/active/.test(this.className)) {
+          this.className = this.className.replace(/active/, '').replace(/\s\s*$/, '');
+        } else {
+          var siblings = tableBodyDavy.getElementsByTagName('a');
+          for (var i = 0; i < siblings.length; i++) {
+            siblings[i].className = siblings[i].className
+              .replace(/active/, '').replace(/\s\s*$/, '');
+          };
+          this.className += ' active';
 
+          // When a menu item is clicked, animate the map to center
+          // its associated marker and open its popup.
+          map.panTo(marker.getLatLng());
+          marker.openPopup();
+        }
+        return false;
+      };
+    }
+  });
+  
   // on each DPW layer append table body
   DPWLayer.eachLayer(function(marker) {
     if (bounds.contains(marker.getLatLng())) {
-      DPWInBounds.push('<tr class="" id='+marker._leaflet_id+'><td>'+marker._leaflet_id+'</td><td>'+marker._leaflet_id+'</td><td>'+marker.feature.properties.name+'</td><td>Another Attribute</td></tr>');
+      var link = tableBodyDPW.appendChild(document.createElement('a'));
+      link.className = 'item';
+      link.href = '#';
+      link.innerHTML = '<tr class="" id='+marker._leaflet_id+'><td>'+marker._leaflet_id+'</td><td>'+marker.feature.properties.name+'</td><td>DPW</td></tr><br>';
+      link.onclick = function() {
+        if (/active/.test(this.className)) {
+          this.className = this.className.replace(/active/, '').replace(/\s\s*$/, '');
+        } else {
+          var siblings = tableBodyDPW.getElementsByTagName('a');
+          for (var i = 0; i < siblings.length; i++) {
+            siblings[i].className = siblings[i].className
+              .replace(/active/, '').replace(/\s\s*$/, '');
+          };
+          this.className += ' active';
+
+          // When a menu item is clicked, animate the map to center
+          // its associated marker and open its popup.
+          map.panTo(marker.getLatLng());
+          marker.openPopup();
+        }
+        return false;
+      };
     }
   });
-  // concatenate html and set it for DPW
-  DPWInBoundsConcat = "";
-  for(i=0; i < DPWInBounds.length; i++) {
-    DPWInBoundsConcat += DPWInBounds[i];
-  }
-  document.getElementById('DPW').innerHTML = DPWInBoundsConcat;
 });
 
 // When map loads, zoom to libraryLayer features
